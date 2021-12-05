fbb2fa4f4a7fe57195e4da3dbf3e1905fe83f891
muhsinG <48914619+muhsinG@users.noreply.github.com>
2021-12-05 18:54:07 +0100

    assignment 2

--- PrototypeApp/adminJS.js
+++ PrototypeApp/adminJS.js
@@ -4,6 +4,9 @@ function initialView(){
     document.getElementById("staffList").style.display="none";
     document.getElementById("studentFormContainer").style.display="none";
     document.getElementById("staffFormContainer").style.display="none";
+    disableFutureDate();
+    dobValidation();
+    joinDateValidation();
 }
 
 function showStudentList(){
@@ -36,4 +39,34 @@ function showStaffForm(){
     document.getElementById("staffList").style.display="block";
     document.getElementById("studentFormContainer").style.display="none";
     document.getElementById("staffFormContainer").style.display="block";
+}
+function getInputDateFormat(date){
+    return date.toISOString().split('T')[0];
+}
+
+function disableFutureDate(){
+    var maxDate = new Date();
+    maxDate.setDate(maxDate.getDate()-1);
+    document.getElementById("StudentDob").setAttribute('max', maxDate.toISOString().split('T')[0]);
+}
+
+function dobValidation(){
+    var today = new Date();
+    var minAge = new Date;
+    var maxAge = new Date;
+    minAge.setFullYear(today.getFullYear()-17);
+    maxAge.setFullYear(today.getFullYear()-60);
+    document.getElementById("StudentDob").setAttribute('max', getInputDateFormat(minAge));
+    document.getElementById("StudentDob").setAttribute('min', getInputDateFormat(maxAge));
+}
+
+function joinDateValidation(){
+    var minJoinDate = new Date();
+    minJoinDate.setFullYear(2015);
+    minJoinDate.setDate(1);
+    minJoinDate.setMonth(1);
+
+    document.getElementById("dateJoined").setAttribute('min', getInputDateFormat(minJoinDate))
+    document.getElementById("dateJoined").setAttribute('max', getInputDateFormat(new Date()))
+
 }
